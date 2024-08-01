8528be869d82c4e2292311de22756cf8297d54dc
david-cornejo <davidcg914@gmail.com>
2024-08-01 12:56:08 -0600

    inicio conexion noticias

---
+++ src/utils/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api', // Asegúrate de que esta URL sea correcta
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
