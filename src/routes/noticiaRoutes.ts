944a48c74485d93305560d0757a8ccdae40edfd6
david-cornejo <davidcg914@gmail.com>
2025-08-29 11:50:58 -0600

    cookie

--- src/routes/noticiaRoutes.ts
+++ src/routes/noticiaRoutes.ts
@@ -1,12 +1,19 @@
-import { Router } from 'express';
-import { getNoticias, createNoticia, updateNoticia, deleteNoticia } from '../service/noticiaService';
-import { authMiddleware } from '../middleware/authMiddleware';
+import { Router } from "express";
+import {
+  getNoticias,
+  getNoticia,
+  createNoticia,
+  updateNoticia,
+  deleteNoticia,
+} from "../service/noticiaService";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.get('/', getNoticias);
-router.post('/', authMiddleware, createNoticia);
-router.put('/:id', authMiddleware, updateNoticia);
-router.delete('/:id', authMiddleware, deleteNoticia);
+router.get("/", getNoticias);
+router.get("/:id", getNoticia);
+router.post("/", authMiddleware, createNoticia);
+router.put("/:id", authMiddleware, updateNoticia);
+router.delete("/:id", authMiddleware, deleteNoticia);
 
 export default router;
--- src/service/noticiaService.ts
+++ src/service/noticiaService.ts
@@ -1,19 +1,19 @@
-import { Request, Response } from 'express';
-import { Noticia } from '../db/models/noticiaModel';
-import { Imagen } from '../db/models/imagenModel';
+import { Request, Response } from "express";
+import { Noticia } from "../db/models/noticiaModel";
+import { Imagen } from "../db/models/imagenModel";
 
 export const getNoticias = async (req: Request, res: Response) => {
   try {
     const noticias = await Noticia.findAll({
-      include: [{ model: Imagen, as: 'imagenes' }]
-    }); 
+      include: [{ model: Imagen, as: "imagenes" }],
+    });
 
-    const noticiasConImagenes = noticias.map(noticia => {
-      const noticiaJSON = noticia.toJSON() as any; 
+    const noticiasConImagenes = noticias.map((noticia) => {
+      const noticiaJSON = noticia.toJSON() as any;
 
       noticiaJSON.imagenes = noticiaJSON.imagenes.map((imagen: any) => ({
         ...imagen,
-        imagen: `${imagen.imagen}`
+        imagen: `${imagen.imagen}`,
       }));
 
       return noticiaJSON;
@@ -21,7 +21,36 @@ export const getNoticias = async (req: Request, res: Response) => {
 
     return res.status(200).json(noticiasConImagenes);
   } catch (error) {
-    return res.status(500).json({ message: 'Error al obtener las noticias', error });
+    return res
+      .status(500)
+      .json({ message: "Error al obtener las noticias", error });
+  }
+};
+
+export const getNoticia = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const noticia = await Noticia.findByPk(id, {
+      include: [{ model: Imagen, as: "imagenes" }],
+    });
+
+    if (!noticia) {
+      return res.status(404).json({ message: "Noticia not found" });
+    }
+
+    const noticiaJSON = noticia.toJSON();
+
+    noticiaJSON.imagenes = noticiaJSON.imagenes.map((imagen: any) => ({
+      ...imagen,
+      imagen: `${imagen.imagen}`,
+    }));
+
+    return res.status(200).json(noticiaJSON);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error al obtener la noticia", error });
   }
 };
 
@@ -32,12 +61,14 @@ export const createNoticia = async (req: Request, res: Response) => {
 
 export const updateNoticia = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const [updated] = await Noticia.update(req.body, { where: { id: Number(id) } });
+  const [updated] = await Noticia.update(req.body, {
+    where: { id: Number(id) },
+  });
   if (updated) {
     const updatedNoticia = await Noticia.findOne({ where: { id: Number(id) } });
     res.status(200).json(updatedNoticia);
   } else {
-    res.status(404).json({ message: 'Noticia not found' });
+    res.status(404).json({ message: "Noticia not found" });
   }
 };
 
@@ -47,6 +78,6 @@ export const deleteNoticia = async (req: Request, res: Response) => {
   if (deleted) {
     res.status(204).send();
   } else {
-    res.status(404).json({ message: 'Noticia not found' });
+    res.status(404).json({ message: "Noticia not found" });
   }
 };
