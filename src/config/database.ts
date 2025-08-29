e919e6248e2e0fe14e570aa9c9b0215963259c5f
Oscaralfaro445 <rot090@hotmail.com>
2025-08-29 11:16:44 -0600

    get noticia

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT!) || 5432,
   dialect: 'postgres',
 });
 
