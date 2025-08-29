a2a226b12b7d438ec58f254db18b86fa17fa8009
Oscaralfaro445 <rot090@hotmail.com>
2025-08-29 11:23:18 -0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:3000"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
