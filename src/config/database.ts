3d708ee888be24e615f689cfc14c3b3489cf28e0
Oscaralfaro445 <rot090@hotmail.com>
2025-08-29 13:58:56 -0600

    database.js

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,16 +1,17 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-<<<<<<< HEAD
-  port: parseInt(process.env.DB_PORT!) || 5432,
-=======
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 15432,
->>>>>>> main
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USERNAME!,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 15432,
+    dialect: "postgres",
+  }
+);
 
 export default sequelize;
