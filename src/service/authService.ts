423e9fd6b637c16359782c18dcf7c2c96fb50db0
david-cornejo <davidcg914@gmail.com>
2025-12-02 11:25:08 -0600

    Remove node_modules from git tracking

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -61,7 +61,7 @@ export const sendRecovery = async(req: Request, res: Response)=>{
     }
     const payload = { sub: user.id };
     const token = jwt.sign(payload, config.jwtSecret, { expiresIn: '15min' });
-    const link = `http://sm8.com.mx/#/change-password?token=${token}`;
+    const link = `https://sm8.com.mx/changePassword?token=${token}`;
     await usuarioService.actualizar(user.id, { recovery_token: token });
     const mail = {
       from: config.email,
