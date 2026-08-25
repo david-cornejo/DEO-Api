1253aa02d21e95af059be0ef28aa4adb73134423
david-cornejo <davidcg914@gmail.com>
2026-08-25 15:48:36 -0600

    fix

--- src/index.ts
+++ src/index.ts
@@ -27,85 +27,85 @@ const setupModels = require("./db/models");
 
 //Produccion
 
-// const app = express();
-// const PORT = process.env.PORT || 3003;
-
-// const corsOptions = {
-//   origin: [
-//     'https://www.sm8.com.mx',
-//     'https://sm8.com.mx',
-//     // Agrega otros subdominios que necesites
-//   ],
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true, // Necesario para cookies cross-origin
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-// };
-
-// const httpsOptions = {
-//   key: fs.readFileSync('./src/certs/privkey.pem'), // Ruta corregida
-//   cert: fs.readFileSync('./src/certs/fullchain.pem'), // Ruta corregida
-// };
-
-// app.use(cors(corsOptions));
-
-// app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-// app.use('/api/noticias', noticiaRoutes);
-// app.use('/api/imagenes', imagenRoutes);
-// app.use('/api/vacantes', vacanteRoutes);
-// app.use('/api/usuarios', usuarioRoutes);
-// app.use('/api/auth', authRoutes);
-//app.use("/api", contactoRoutes);
-// app.use('/api/catalogo', catalogoRoutes);
-
-// setupModels(sequelize);
-
-// sequelize.authenticate().then(() => {
-//   https.createServer(httpsOptions, app).listen(PORT, () => {
-//     console.log(`HTTPS Server is running on port ${PORT}`);
-//   });
-// }).catch((err) => {
-//   console.error('Unable to connect to the database:', err);
-// });
-
-// Desarrollo|
-
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Cambia esto al origen de tu frontend
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Permite cookies y headers de autenticación
+  origin: [
+    'https://www.sm8.com.mx',
+    'https://sm8.com.mx',
+    // Agrega otros subdominios que necesites
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Necesario para cookies cross-origin
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
 
+const httpsOptions = {
+  key: fs.readFileSync('./src/certs/privkey.pem'), // Ruta corregida
+  cert: fs.readFileSync('./src/certs/fullchain.pem'), // Ruta corregida
+};
+
 app.use(cors(corsOptions));
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use("/api/noticias", noticiaRoutes);
-app.use("/api/imagenes", imagenRoutes);
-app.use("/api/vacantes", vacanteRoutes);
-app.use("/api/usuarios", usuarioRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/noticias', noticiaRoutes);
+app.use('/api/imagenes', imagenRoutes);
+app.use('/api/vacantes', vacanteRoutes);
+app.use('/api/usuarios', usuarioRoutes);
+app.use('/api/auth', authRoutes);
 app.use("/api", contactoRoutes);
-app.use("/api/catalogo", catalogoRoutes);
+app.use('/api/catalogo', catalogoRoutes);
 
 setupModels(sequelize);
 
-sequelize
-  .authenticate()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
+sequelize.authenticate().then(() => {
+  https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log(`HTTPS Server is running on port ${PORT}`);
   });
+}).catch((err) => {
+  console.error('Unable to connect to the database:', err);
+});
+
+// Desarrollo|
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// const corsOptions = {
+//   origin: "http://localhost:3000", // Cambia esto al origen de tu frontend
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true, // Permite cookies y headers de autenticación
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
+
+// app.use(cors(corsOptions));
+
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+// app.use("/api/noticias", noticiaRoutes);
+// app.use("/api/imagenes", imagenRoutes);
+// app.use("/api/vacantes", vacanteRoutes);
+// app.use("/api/usuarios", usuarioRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api", contactoRoutes);
+// app.use("/api/catalogo", catalogoRoutes);
+
+// setupModels(sequelize);
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Unable to connect to the database:", err);
+//   });
   
